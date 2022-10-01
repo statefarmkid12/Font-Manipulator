c6e65ba98b138199c0675045dcae4130d121586a
statefarmkid12 <110681569+statefarmkid12@users.noreply.github.com>
2022-10-01 14:44:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 function setup(){
-    canvas = createCanvas(1000,925);
-    canvas.center();
+    canvas = createCanvas(800,620);
+    canvas.position(1000,300);
     video = createCapture(VIDEO);
     video.size(1000,925);
+
+    poseNet = ml5.poseNet(video, modelActivated);
+    poseNet.on("pose", gotPoses);
+};
+
+function modelActivated(){
+    console.log("PoseNet has been launched")
+}
+
+rWrist = 0
+lWrist = 0
+Nose_x = 0
+Nose_y = 0
+
+function gotPoses(results){
+    if(results > 0){
+        console.log(result);
+        lWrist = results[0].pose.leftWrist.x;
+        rWrist = results[0].pose.rightWrist.x;
+        Nose_x = results[0].pose.nose.x;
+        Nose_y = results[0].pose.nose.y;
+    }
 }
 
 function draw(){
-    image(video, 0, 0, 1000, 925);
-}
+    
+};
